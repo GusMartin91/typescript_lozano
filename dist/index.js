53ec634724ea81e5c524fd29afbaa69ecd275885
GusMartin91 <gustavomlozano@gmail.com>
2023-11-07 09:53:53 -0300

    Ejercicio completo

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,47 @@
 "use strict";
 let mensaje = "hola Mundo";
 mensaje = "chanchito feliz";
-console.log(mensaje);
 mensaje = "chau mundo";
+console.log(mensaje);
+console.log(typeof []);
+let extincionDinosaurios = 76000000;
+let dinosaurioFavorito = "Triceratops";
+let extintos = true;
+function chanchitoFeliz(config) {
+    return config * config;
+}
+console.log(chanchitoFeliz(23));
+let animales = ["chanchito", "perrito", "conejito"];
+let nums = [1, 2, 3];
+let checks = [];
+let nums2 = [];
+let animalesMayuscula = animales.map((x) => x.toUpperCase());
+console.log(animalesMayuscula);
+let tupla = [1, "chanchito feliz"];
+const chica = "s";
+const mediana = "m";
+const grande = "l";
+const extraGrande = "xl";
+var Talla;
+(function (Talla) {
+    Talla["Chica"] = "s";
+    Talla["Mediana"] = "m";
+    Talla["Grande"] = "l";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+const variable1 = Talla.Grande;
+console.log(variable1);
+const estado = 2;
+const objeto = {
+    id: 1,
+    nombre: "",
+    talla: Talla.Mediana,
+    direccion: {
+        numero: 123,
+        calle: "Siempre Viva",
+        pais: "U.S.A.",
+    },
+};
+objeto.nombre = "Carlitos";
+console.log(objeto);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,80 @@ let mensaje: string = "hola Mundo";
 
 mensaje = "chanchito feliz";
 
+mensaje = "chau mundo";
+
 console.log(mensaje);
 
-mensaje = "chau mundo";
+console.log(typeof []);
+
+let extincionDinosaurios = 76_000_000;
+let dinosaurioFavorito = "Triceratops";
+let extintos = true;
+
+function chanchitoFeliz(config: number) {
+  return config * config;
+}
+console.log(chanchitoFeliz(23));
+
+let animales: string[] = ["chanchito", "perrito", "conejito"];
+let nums: number[] = [1, 2, 3];
+let checks: boolean[] = [];
+let nums2: Array<number> = [];
+
+let animalesMayuscula = animales.map((x) => x.toUpperCase());
+console.log(animalesMayuscula);
+
+let tupla: [number, string] = [1, "chanchito feliz"];
+
+const chica = "s";
+const mediana = "m";
+const grande = "l";
+const extraGrande = "xl";
+
+// PascalCase
+enum Talla {
+  Chica = "s",
+  Mediana = "m",
+  Grande = "l",
+  ExtraGrande = "xl",
+}
+
+const variable1 = Talla.Grande;
+
+console.log(variable1);
+
+const enum LoadingStates {
+  Idle,
+  Loading,
+  Success,
+  Error,
+}
+
+const estado = LoadingStates.Success;
+
+type Direccion = {
+  numero: number;
+  calle: string;
+  pais: string;
+};
+
+type Persona = {
+  readonly id: number;
+  nombre: string;
+  talla: Talla;
+  direccion: Direccion;
+};
+
+const objeto: Persona = {
+  id: 1,
+  nombre: "",
+  talla: Talla.Mediana,
+  direccion: {
+    numero: 123,
+    calle: "Siempre Viva",
+    pais: "U.S.A.",
+  },
+};
+objeto.nombre = "Carlitos";
+
+console.log(objeto);
